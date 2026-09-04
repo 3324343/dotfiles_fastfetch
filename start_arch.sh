58167292889a8392df099766d85877a0f70f0fa6
3324343 <146524790+3324343@users.noreply.github.com>
2026-09-04 11:31:19 +0700

    Create start_arch.sh

---
+++ start_arch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mnt="/data/local/tmp/chrootarch"
+busybox mount -o remount,dev,suid /data
+
+mount -o bind /dev $mnt/dev/
+busybox mount -t proc proc $mnt/proc/
+busybox mount -t sysfs sysfs $mnt/sys/
+busybox mount -t devpts devpts $mnt/dev/pts/
+busybox mount -o bind /sdcard $mnt/media/sdcard
+busybox mount -t tmpfs /cache $mnt/var/cache
+
+# /dev/shm for Electron apps
+busybox mount -t tmpfs -o size=256M tmpfs $mnt/dev/shm
+
+# chroot into Arch
+busybox chroot $mnt /bin/su - root
